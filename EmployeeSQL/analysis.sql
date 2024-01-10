821a535e13f88e9c43fe02ee440c1b2b632c7aed
Amarilli <147782731+Amarilli@users.noreply.github.com>
2024-01-10 12:24:41 -0600

    final code

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -1,5 +1,6 @@
  
 --List the employee number, last name, first name, sex, and salary of each employee.
+
 SELECT 
     emplo.emp_no,
     emplo.last_name,
@@ -12,6 +13,7 @@ JOIN
     Salaries AS sala ON emplo.emp_no = sala.emp_no;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
+
 SELECT 
     first_name,
     last_name,
@@ -22,6 +24,7 @@ WHERE
     EXTRACT(YEAR FROM hire_date) = 1986;
 	
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
 SELECT 
     dm.dept_no AS department_number,
     d.dept_name AS department_name,
@@ -36,6 +39,7 @@ JOIN
     Employees AS e ON dm.emp_no = e.emp_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
 SELECT 
     de.dept_no AS department_number,
     e.emp_no AS employee_number,
@@ -50,6 +54,7 @@ JOIN
     Departments AS d ON de.dept_no = d.dept_no;
 	
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
 SELECT 
     first_name,
     last_name,
@@ -61,6 +66,7 @@ WHERE
     AND last_name LIKE 'B%';
 	
 -- List each employee in the Sales department, including their employee number, last name, and first name
+
 SELECT 
     e.emp_no AS employee_number,
     e.last_name,
@@ -75,6 +81,7 @@ WHERE
     d.dept_name = 'Sales';
 	
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
 SELECT 
     e.emp_no AS employee_number,
     e.last_name,
@@ -90,6 +97,7 @@ WHERE
     d.dept_name IN ('Sales', 'Development');
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
 SELECT 
     last_name,
     COUNT(*) AS frequency_count
